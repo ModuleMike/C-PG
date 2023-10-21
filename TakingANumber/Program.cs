531ab4d1bcacef5cb53c831f1e2e991db52b8598
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-21 00:00:32 -0700

    Add files via upload

---
+++ TakingANumber/Program.cs
@@ -0,0 +1,35 @@
+﻿int AskForNumber(string text)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int output))
+        {
+            return output;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Try Again");
+        }
+        
+    } 
+}
+
+int AskForNumberInRange( string text, int min, int max)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int output) && output > min && output < max)
+        {
+            return output;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Try Again");
+        }
+    }
+}
+
+AskForNumber("What is the airspeed velocity of an unladen swallow? ");
+AskForNumberInRange("What about in he range of 1 - 120?", 1, 121);
