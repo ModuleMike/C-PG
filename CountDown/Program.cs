3fcde576a43a7cd0ca74d301147c8044212d1fd2
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-26 23:23:22 -0700

    Add files via upload

---
+++ CountDown/Program.cs
@@ -0,0 +1,18 @@
+﻿for( int x = 10; x > 0; x--)
+{
+    Console.WriteLine(x);
+}
+
+int CountDown(int number)
+{
+    if(number == 1)
+    {
+        Console.WriteLine(number);
+        return 1;
+    }
+    Console.WriteLine(number);
+    return CountDown(number - 1);
+    
+}
+
+CountDown(20);
