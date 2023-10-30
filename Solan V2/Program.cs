22ca999caa22314649c0d02402531dd3b4fb8fdb
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-29 23:34:57 -0700

    Add files via upload

---
+++ Solan V2/Program.cs
@@ -0,0 +1,21 @@
+﻿for (int number = 1; number <= 100; number++)
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{number}: Electric & Fire!!");
+    }
+    else if ( number % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{number}: Fire!!");
+    }
+    else if ( number % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{number}: Electric!!");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"{number}: Normal!!");
+    }
