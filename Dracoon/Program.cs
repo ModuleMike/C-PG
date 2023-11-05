d0d155bf954b8044cb4bc5ac4200a9b2890a413a
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-11-04 23:07:15 -0700

    Add files via upload

---
+++ Dracoon/Program.cs
@@ -0,0 +1,100 @@
+﻿// Character Type
+char varChar = 'm';
+Console.WriteLine(varChar);
+varChar = 'l';
+Console.WriteLine(varChar);
+
+// String Type
+string varString = "String";
+Console.WriteLine(varString);
+varString = "New String";
+Console.WriteLine(varString);
+
+// Integar Types
+
+// Byte
+byte varByte = 1;
+Console.WriteLine(varByte);
+varByte = 2;
+Console.WriteLine(varByte);
+
+// Short
+short varShort = 2;
+Console.WriteLine(varShort);
+varShort = 3;
+Console.WriteLine(varShort);
+
+// Int
+int varInt = 3;
+Console.WriteLine(varInt);
+varInt = 4;
+Console.WriteLine(varInt);
+
+// Long
+long varLong = 4;
+Console.WriteLine(varLong);
+varLong = 5;
+Console.WriteLine(varLong);
+
+// Sbyte
+sbyte varSByte = -1;
+Console.WriteLine(varSByte);
+varSByte = -2;
+Console.WriteLine(varSByte);
+
+// UShort
+ushort varUShort = 65_534;
+Console.WriteLine(varUShort);
+varUShort = 56_433;
+Console.WriteLine(varUShort);
+
+// UInt
+uint varUINT = 10090291;
+Console.WriteLine(varUINT);
+varUINT = 314142142;
+Console.WriteLine(varUINT);
+
+// ULong
+ulong varULong = 210931321903201994;
+Console.WriteLine(varULong);
+varULong = 121313131;
+Console.WriteLine(varULong);
+
+// Floating-Point Options
+
+// Float
+float varFloat = 1.123f;
+Console.WriteLine(varFloat);
+varFloat = 1.21414f;
+Console.WriteLine(varFloat);
+
+// Double
+double varDouble = 2.12123;
+Console.WriteLine(varDouble);
+varDouble = 12214.2121;
+Console.WriteLine(varDouble);
+
+// Decimal 
+decimal varDecimal = 3.75M;
+Console.WriteLine(varDecimal);
+varDecimal = 3.523m;
+Console.WriteLine(varDecimal);
+
+// Bianry
+char aLetter = '\u0061';
+Console.WriteLine(aLetter);
+
+// Hexidecimal
+int varHex = 0XFF00FF;
+Console.WriteLine(varHex);
+
+// Boolean
+bool varBool = true;
+Console.WriteLine(varBool);
+varBool = false;
+Console.WriteLine(varBool);
+
+
+
+
+
