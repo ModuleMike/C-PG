273c4016b3f67f94b8dd0bfd1b0d5607f5e9cc25
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-11-18 21:47:19 -0800

    Add files via upload

---
+++ SimulasSoupv2/App.cs
@@ -0,0 +1,16 @@
+﻿namespace SimulasSoup
+{
+    internal class App
+    {
+        public static void Run()
+        {
+            while (true)
+            {
+                ConsoleIO.Menu();
+                (BowlType, MeatVeggie, Spice) orderMeal = (ConsoleIO.BowlChoice(), ConsoleIO.MeatVeggieChoice(), ConsoleIO.SpiceChoice());
+                Order.ReceiptMeal(orderMeal);
+                break;
+            }
+        }
+    }
+}
---
+++ SimulasSoupv2/Food.cs
@@ -0,0 +1,23 @@
+﻿namespace SimulasSoup
+{
+    public enum BowlType
+    {
+        soup,
+        stew,
+        gumbo
+    }
+    public enum MeatVeggie
+    {
+        mushrooms,
+        chicken,
+        carrots,
+        potatoes
+    }
+    public enum Spice
+    {
+        spicy,
+        salty,
+        sweet
+    }
+}
+
