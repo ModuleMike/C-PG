41b52272ae00e7a6dcc1a21bd5f41ff97428d8be
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-11-20 00:00:15 -0800

    Add files via upload

---
+++ ContactClass/Contact.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactClass
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+
+        private DateTime _birthDate;
+        public DateTime Birthdate
+        {
+            get { return _birthDate; }
+            set
+            {
+                if (value > DateTime.Today)
+                {
+                    _birthDate = DateTime.Today;
+                }
+                else
+                {
+                    _birthDate = value;
+                }
+            }
+        }
+        public string NameLastFisrt
+        {
+            get { return $"{LastName}, {FirstName}"; }
+        }
+        public int AgeYears
+        {
+            get
+            {
+                int age = DateTime.Today.Year - Birthdate.Year;
+
+                if (Birthdate.AddYears(age) > DateTime.Today)
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+    }
+}
---
+++ ContactClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using ContactClass;
+
+Contact c1 = new Contact();
+
+c1.FirstName = "Joe";
+c1.LastName = "Schmoe";
+c1.Birthdate = new DateTime(1997, 2, 15);
+
+Console.WriteLine($"{c1.NameLastFisrt} | Age: {c1.AgeYears}");
+
+Contact c2 = new Contact()
+{
+    FirstName = "Sue",
+    LastName = "Miller",
+    Birthdate = new DateTime(1997, 6, 15)
+};
+
+Console.WriteLine($"{c2.NameLastFisrt} | Age: {c2.AgeYears}");
