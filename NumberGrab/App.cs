3c9c056e5baf61a2c76ad7ae84a7aef4c9100db3
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-11-28 22:57:46 -0800

    Add files via upload

---
+++ NumberGrab/App.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace GuessingGame
+{
+    public class App
+    {
+
+        public static void Run()
+        {
+            GameManager manager = new GameManager();
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Welcome to the Guessing Game!");
+                int maxGuess = ConsoleIO.GetMaxNumber();
+
+                manager.GenerateNumber(maxGuess);
+
+                Console.WriteLine($"I'm thinking of a number between 1 and {maxGuess}. Can you Guess it?");
+                int guesscount = 0;
+                bool guessed = false;
+
+                while (!guessed)
+                {
+                    int guess = ConsoleIO.GetGuess(maxGuess);
+                    guesscount++;
+                    guessed = manager.ParseGuess(guess);
+                }
+
+                Console.WriteLine($"It took you {guesscount} guesses.");
+
+            } while (ConsoleIO.PlayAgain());
+
+          
+        }
+
+    }
+}
---
+++ NumberGrab/GameManager.cs
@@ -0,0 +1,33 @@
+﻿
+namespace GuessingGame
+{
+    public class GameManager
+    {
+        private int _numberToGuess;
+
+        public void GenerateNumber(int maxGuess)
+        {
+            Random random = new Random();
+            _numberToGuess = random.Next(1, maxGuess + 1);
+        }
+
+        public bool ParseGuess(int guess)
+        {
+            if (guess < _numberToGuess)
+            {
+                Console.WriteLine("Higher!");
+                return false;
+            }
+            else if (guess > _numberToGuess)
+            {
+                Console.WriteLine("Lower!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("You Got It!");
+                return true;
+            }
+        }
+    }
+}
