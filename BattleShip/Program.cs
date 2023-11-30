e24287f2a435a5031ca3da9c8cdda3ae8bf55c55
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-11-29 23:47:27 -0800

    Add files via upload

---
+++ BattleShip/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = AskForNumberInRange("Player 1 Guess A Number Between 1 - 100: ", 1, 100);
+
+Console.Clear();
+
+Console.WriteLine("Player 2 it is your turn to try and guess the number...");
+
+while (true)
+{
+    int guess = AskForNumber("Guess the number... ");
+    if (guess < number) Console.WriteLine($"{guess} is to low!!");
+    else if (guess > number) Console.WriteLine($"{guess} is to High!!");
+    else break;
+}
+
+Console.WriteLine($"{number} is the the right number!!");
+
+int AskForNumberInRange(String text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber(text);
+        if (number >= min && number <= max)
+        return number;
+    }
+
+}
+
+int AskForNumber(string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
